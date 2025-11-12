9e83eaab5c37d79a8f5b4d2fed22ee8fc73662eb
ahmaddanifd-web <ahmaddanifd@gmail.com>
2025-11-12 14:30:03 +0700

    Fix logout functionality with custom view

--- core/views.py
+++ core/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth import logout
+from django.views.decorators.http import require_http_methods
+from django.http import HttpResponseRedirect
 from .models import Layanan
 
 
 def home(request):
     layanan = Layanan.objects.all()[:6]
     return render(request, 'home.html', {'layanan': layanan})
+
+
+@require_http_methods(["GET", "POST"])
+def custom_logout(request):
+    logout(request)
+    return render(request, 'registration/logged_out.html')
