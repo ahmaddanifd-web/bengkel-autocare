8a8aafdcc2abff517bdf011557e0356f7d7cb4c1
ahmaddanifd-web <ahmaddanifd@gmail.com>
2025-11-12 14:21:05 +0700

    Add registration page and auth flow

--- bengkel/urls.py
+++ bengkel/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,6 +10,7 @@ urlpatterns = [
     path('booking/', include('booking.urls')),
     path('payment/', include('payment.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('register/', TemplateView.as_view(template_name='registration/register.html'), name='register'),
 ]
 
 if settings.DEBUG:
